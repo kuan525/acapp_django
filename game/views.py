599c03e524798e45de0eebf75ecf0fe7d08fbf09
kuan525 <kuan_525@163.com>
2022-01-21 03:38:26 +0800

    step1

--- game/views.py
+++ game/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):
+    line1 = '<h1 style="text-align:center">我的第一个网页</h1>'
+    line2 = '<img src="https://z3.ax1x.com/2021/08/25/hV0alT.jpg" width = 950>'
+    return HttpResponse(line1 + line2)
