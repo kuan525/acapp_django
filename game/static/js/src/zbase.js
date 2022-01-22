b42c1bc38e3dcf0717939ce179b66127f98d24a3
kuan525 <kuan_525@163.com>
2022-01-23 01:37:43 +0800

    write menu and playground scene

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,5 +1,14 @@
-cladd AcGame {
+class AcGame {
     constructor(id){
-        
+        // console.log("kuan");
+        this.id = id;
+        this.$ac_game = $('#' + id);
+        this.menu = new AcGameMenu(this);
+        this.playground = new AcGamePlayground(this);
+
+        this.start();
+    }
+
+    start() {
     }
 }
--- game/urls/index.py
+++ game/urls/index.py
@@ -1,9 +1,9 @@
-form django.urls import path, include
-form game.views.index import index
+from django.urls import path, include
+from game.views.index import index
 
 urlpatterns = [
-    path("", index, name="index")
-    path("menu/", inlclude("game.urls.menu.index")),
-    path("playground/", inlcude("game.urls.palyground.index")),
+    path("", index, name="index"),
+    path("menu/", include("game.urls.menu.index")),
+    path("playground/", include("game.urls.playground.index")),
     path("settings/", include("game.urls.settings.index")),
 ]
